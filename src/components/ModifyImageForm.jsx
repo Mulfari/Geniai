0953ee71fe8e402ea35bcbb31915b9fa60d2ab8e
Salvador Mulfari <josesalvador0102@gmail.com>
2023-03-31 21:54:37 -0400

    Prueba

--- src/components/ModifyImageForm.jsx
+++ src/components/ModifyImageForm.jsx
@@ -1,110 +1,61 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './styles.css/ModifyImageForm.css';
 
 const ModifyImageForm = () => {
+  const [imageURL, setImageURL] = useState('');
   const [description, setDescription] = useState('');
-  const [originalImage, setOriginalImage] = useState(null);
-  const [modifiedImage, setModifiedImage] = useState(null);
-  const [loading, setLoading] = useState(false);
 
-  const uploadOriginalImage = async (event) => {
-    setLoading(true);
+  const getImageDescription = async () => {
+    // Asegúrate de que la URL de la API sea correcta
+    const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+    // Agrega tus credenciales de API aquí
+    const apiKey = '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';
 
     try {
-      const file = event.target.files[0];
+      const response = await axios.post(apiUrl, {
+        prompt: `Describe la siguiente imagen: ${imageURL}`,
+        max_tokens: 50,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`
+        }
+      });
 
-      // Read the file data as a base64 encoded string
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onloadend = async () => {
-        const base64data = reader.result;
-
-        // Upload the image to OpenAI's DALL-E 2 API
-        const response = await axios.post(
-          'https://api.openai.com/v1/images/generations',
-          {
-            model: 'image-alpha-001',
-            image: base64data.split(',')[1], // The base64 encoded image data (without the "data:image/jpeg;base64," prefix)
-            size: "512x512",
-            response_format: 'url',
-          },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer YOUR_API_KEY',
-            },
-          }
-        );
-
-        // Set the original image and clear the description
-        setOriginalImage(response.data.data[0].url);
-        setDescription('');
-      };
+      setDescription(response.data.choices[0].text);
     } catch (error) {
-      console.error('Error uploading image:', error);
-    } finally {
-      setLoading(false);
+      console.error('Error al obtener la descripción de la imagen:', error);
     }
   };
 
-  const modifyImage = async () => {
-    setLoading(true);
-
-    try {
-      // Upload the modified image to OpenAI's DALL-E 2 API
-      const response = await axios.post(
-        'https://api.openai.com/v1/images/generations',
-        {
-          model: 'image-alpha-001',
-          prompt: description,
-          image: modifiedImage.split(',')[1], // The base64 encoded image data (without the "data:image/jpeg;base64," prefix)
-          size: "512x512",
-          response_format: 'url',
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer YOUR_API_KEY',
-          },
-        }
-      );
-
-      // Set the modified image and clear the description
-      setModifiedImage(response.data.data[0].url);
-      setDescription('');
-    } catch (error) {
-      console.error('Error generating modified image:', error);
-    } finally {
-      setLoading(false);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getImageDescription();
   };
 
   return (
-    <div>
-    <h1>Modificador de imágenes con DALL-E 2</h1>
-    <input
-    type="text"
-    placeholder="URL de la imagen a modificar"
-    value={imageUrl}
-    onChange={(e) => setImageUrl(e.target.value)}
-    />
-    <input
-    type="text"
-    placeholder="Descripción de la modificación"
-    value={modificationDescription}
-    onChange={(e) => setModificationDescription(e.target.value)}
-    />
-    <button onClick={modifyImage} disabled={loading}>
-    {loading ? 'Modificando...' : 'Modificar imagen'}
-    </button>
-    {imageSrc && (
-    <div>
-    <h2>Imagen modificada:</h2>
-    <img src={imageSrc} alt="Imagen modificada" />
+    <div className="modify-image-form">
+      <h2>Modificar imagen</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="image-url">URL de la imagen:</label>
+        <input
+          type="text"
+          id="image-url"
+          value={imageURL}
+          onChange={(e) => setImageURL(e.target.value)}
+        />
+        <button type="submit">Obtener descripción</button>
+      </form>
+      {description && (
+        <div className="image-description">
+          <h3>Descripción de la imagen:</h3>
+          <p>{description}</p>
+        </div>
+      )}
     </div>
-    )}
-    </div>
-    );
-    };
-    
-    export default ModifyImageForm;
+  );
+};
+
+export default ModifyImageForm;
