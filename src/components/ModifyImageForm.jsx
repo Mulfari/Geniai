689cf940f9f494af3bfa3f1aa5374ef1d837ec69
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-01 02:41:36 -0400

    probando

--- src/components/ModifyImageForm.jsx
+++ src/components/ModifyImageForm.jsx
@@ -1,61 +1,107 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
-import './styles.css/ModifyImageForm.css';
 
 const ModifyImageForm = () => {
-  const [imageURL, setImageURL] = useState('');
+  const [imageFile, setImageFile] = useState(null);
   const [description, setDescription] = useState('');
 
-  const getImageDescription = async () => {
-    // Asegúrate de que la URL de la API sea correcta
-    const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+  const handleImageChange = (e) => {
+    setImageFile(e.target.files[0]);
+  };
 
-    // Agrega tus credenciales de API aquí
-    const apiKey = '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';
+  const getImageDescription = async (e) => {
+    e.preventDefault();
+
+    // Asegúrate de agregar tus credenciales de API correctas
+    const clarifaiApiKey = 'a270745c80654ce085dc1b12c1415227';
+    const openaiApiKey = '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';
+
+    // Crear un objeto FormData para enviar la imagen
+    const formData = new FormData();
+    formData.append('image', imageFile);
 
     try {
-      const response = await axios.post(apiUrl, {
-        prompt: `Describe la siguiente imagen: ${imageURL}`,
-        max_tokens: 50,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
-        }
-      });
+      // Convierte la imagen a base64
+      const reader = new FileReader();
+      reader.readAsDataURL(imageFile);
+      reader.onloadend = async () => {
+        const base64Image = reader.result.split(',')[1];
 
-      setDescription(response.data.choices[0].text);
+        // Utiliza Clarifai API para extraer etiquetas de la imagen
+        const clarifaiResponse = await axios.post(
+          'https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/output',
+          {
+            inputs: [
+              {
+                data: {
+                  image: {
+                    base64: base64Image,
+                  },
+                },
+              },
+            ],
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Key ${clarifaiApiKey}`,
+            },
+          }
+        );
+
+        const labels = clarifaiResponse.data.outputs[0].data.concepts
+          .map((concept) => concept.name)
+          .join(', ');
+
+        // Utiliza OpenAI API para generar una descripción basada en las etiquetas de la imagen
+        const openaiResponse = await axios.post(
+          'https://api.openai.com/v1/engines/davinci-codex/completions',
+          {
+            prompt: `Describe una imagen que contiene ${labels}:`,
+            max_tokens: 50,
+            n: 1,
+            stop: null,
+            temperature: 0.7,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${openaiApiKey}`,
+            },
+          }
+        );
+
+        setDescription(openaiResponse.data.choices[0].text.trim());
+      };
     } catch (error) {
-      console.error('Error al obtener la descripción de la imagen:', error);
+      console.error('Error al obtener la descripción de la imagen:',error);
     }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    getImageDescription();
-  };
-
-  return (
-    <div className="modify-image-form">
-      <h2>Modificar imagen</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="image-url">URL de la imagen:</label>
-        <input
-          type="text"
-          id="image-url"
-          value={imageURL}
-          onChange={(e) => setImageURL(e.target.value)}
+    };
+    
+    return (
+    <div>
+    <h2>Modificar imagen</h2>
+    <form onSubmit={getImageDescription}>
+    <div>
+    <label htmlFor="image">Cargar imagen:</label>
+    <input
+          type="file"
+          name="image"
+          id="image"
+          accept="image/*"
+          onChange={handleImageChange}
         />
-        <button type="submit">Obtener descripción</button>
-      </form>
-      {description && (
-        <div className="image-description">
-          <h3>Descripción de la imagen:</h3>
-          <p>{description}</p>
-        </div>
-      )}
     </div>
-  );
-};
-
-export default ModifyImageForm;
+    <button type="submit">Obtener descripción</button>
+    </form>
+    {description && (
+    <div>
+    <h3>Descripción de la imagen:</h3>
+    <p>{description}</p>
+    </div>
+    )}
+    </div>
+    );
+    };
+    
+    export default ModifyImageForm;
