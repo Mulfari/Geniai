1abc666b8d531850947cb7d56bc9d0b280a89398
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-04 13:54:55 -0400

    sad

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -15,7 +15,7 @@ const ChatComponent = () => {
     };
 
     try {
-      const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/chat/completions', data, {
+      const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', data, {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
