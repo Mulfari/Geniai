8334a08342cea26b844532b5e4fc552a0d30b4c9
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-04 14:37:29 -0400

    as2

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,37 +1,43 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const ChatComponent = () => {
   const [message, setMessage] = useState("");
-  const [response, setResponse] = useState("");
+  const [chatLog, setChatLog] = useState([]);
 
   const sendMessage = async () => {
     try {
-      const { data } = await axios.post('https://api.openai.com/v1/engine/<ENGINE_ID>/completions', {
-        prompt: message,
+      const response = await axios.post("https://api.openai.com/v1/engines/davinci-codex/completions", {
+        prompt: `User: ${message}\nAI:`,
         max_tokens: 50,
-        temperature: 0.5,
-        n: 1,
-        stop: "\n"
+        temperature: 0.5
       }, {
         headers: {
-          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
         }
       });
-      setResponse(data.choices[0].text);
+      setChatLog(prevState => [...prevState, { user: message, ai: response.data.choices[0].text }]);
+      setMessage("");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
     <div>
+      <div>
+        {chatLog.map((log, index) => (
+          <div key={index}>
+            <div>{log.user}</div>
+            <div>{log.ai}</div>
+          </div>
+        ))}
+      </div>
       <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
       <button onClick={sendMessage}>Send</button>
-      <p>{response}</p>
     </div>
   );
-}
+};
 
 export default ChatComponent;
