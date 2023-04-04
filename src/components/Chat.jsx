fdd92ae5bc12c1aa8b46715e2a510b180a6eff72
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-04 14:21:11 -0400

    asd

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './ChatComponent.css';
+import './styles.css/Chat.css';
 
 const ChatComponent = () => {
   const [messages, setMessages] = useState([]);
