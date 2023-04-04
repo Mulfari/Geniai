6d6cde552023d28252df309421e9661137aa46ce
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-04 15:46:16 -0400

    asd

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -8,25 +8,25 @@ const ChatComponent = () => {
 
   const sendMessage = async () => {
     try {
-      const response = await axios.post("https://api.openai.com/v1/engines/davinci/completions", {
-        prompt: `User: ${message}\nAI:`,
-        max_tokens: 100,
-        n: 1,
-        stop: "\n",
-        temperature: 0.5
-      }, {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: message }]
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          }
         }
-      });
-      setChatLog(prevState => [...prevState, { user: message, ai: response.data.choices[0].text }]);
+      );
+      setChatLog(prevState => [...prevState, { user: message, ai: response.data.choices[0].message.content }]);
       setMessage("");
     } catch (error) {
       console.log(error);
     }
   };
-
   return (
     <div className="chat-container">
       <div className="chat-box">
