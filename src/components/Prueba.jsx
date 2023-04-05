2f123b2f0f0d67aa5f177ab0ef061798cc759b37
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-05 17:09:26 -0400

    asd

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -24,7 +24,7 @@ const ImageVariation = () => {
     try {
       const response = await axios.post('https://api.openai.com/v1/images/variations', formData, {
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
           'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
         },
         params: {
