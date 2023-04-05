3d02902ced6572b589d2b5cbfccf97f67e396cd9
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-05 17:25:39 -0400

    2

--- src/components/Prueba2.jsx
+++ src/components/Prueba2.jsx
@@ -4,16 +4,11 @@ import axios from 'axios';
 
 const ImageEdit = () => {
   const [image, setImage] = useState(null);
-  const [mask, setMask] = useState(null);
   const [prompt, setPrompt] = useState('');
   const [editedImage, setEditedImage] = useState(null);
 
   const handleImageChange = (e) => {
     setImage(e.target.files[0]);
-  };
-
-  const handleMaskChange = (e) => {
-    setMask(e.target.files[0]);
   };
 
   const handlePromptChange = (e) => {
@@ -28,25 +23,33 @@ const ImageEdit = () => {
       return;
     }
 
-    const formData = new FormData();
-    formData.append('image', image);
-    if (mask) {
-      formData.append('mask', mask);
-    }
-    formData.append('prompt', prompt);
+    const imageToBase64 = (file) =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+
+    const base64Image = await imageToBase64(image);
 
     try {
-      const response = await axios.post('https://api.openai.com/v1/images/edits', formData, {
-        headers: {
-          'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-          'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-        },
-        params: {
+      const response = await axios.post(
+        'https://api.openai.com/v1/images/edits',
+        {
+          image: base64Image,
+          prompt: prompt,
           n: 1,
           size: '1024x1024',
           response_format: 'url',
         },
-      });
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          },
+        }
+      );
 
       setEditedImage(response.data.data[0].url);
     } catch (error) {
@@ -58,8 +61,13 @@ const ImageEdit = () => {
     <div>
       <form onSubmit={handleSubmit}>
         <input type="file" accept="image/png" onChange={handleImageChange} />
-        <input type="file" accept="image/png" onChange={handleMaskChange} />
-        <input type="text" value={prompt} onChange={handlePromptChange} maxLength="1000" placeholder="Escribe una descripción" />
+        <input
+          type="text"
+          value={prompt}
+          onChange={handlePromptChange}
+          maxLength="1000"
+          placeholder="Escribe una descripción"
+        />
         <button type="submit">Generar imagen editada</button>
       </form>
       {editedImage && (
