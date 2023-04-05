b2f1209724576d759a2a3bc51ceff9e50ef030b3
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-05 17:22:28 -0400

    asd

--- src/components/Prueba2.jsx
+++ src/components/Prueba2.jsx
@@ -1,28 +1,42 @@
-// ImageVariation.js
+// ImageEdit.js
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const ImageVariation = () => {
+const ImageEdit = () => {
   const [image, setImage] = useState(null);
-  const [generatedImage, setGeneratedImage] = useState(null);
+  const [mask, setMask] = useState(null);
+  const [prompt, setPrompt] = useState('');
+  const [editedImage, setEditedImage] = useState(null);
 
-  const handleChange = (e) => {
+  const handleImageChange = (e) => {
     setImage(e.target.files[0]);
+  };
+
+  const handleMaskChange = (e) => {
+    setMask(e.target.files[0]);
+  };
+
+  const handlePromptChange = (e) => {
+    setPrompt(e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!image) {
-      alert('Por favor, selecciona una imagen.');
+    if (!image || !prompt) {
+      alert('Por favor, selecciona una imagen y escribe un texto.');
       return;
     }
 
     const formData = new FormData();
     formData.append('image', image);
+    if (mask) {
+      formData.append('mask', mask);
+    }
+    formData.append('prompt', prompt);
 
     try {
-      const response = await axios.post('https://api.openai.com/v1/images/variations', formData, {
+      const response = await axios.post('https://api.openai.com/v1/images/edits', formData, {
         headers: {
           'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
           'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
@@ -34,26 +48,28 @@ const ImageVariation = () => {
         },
       });
 
-      setGeneratedImage(response.data.data[0].url);
+      setEditedImage(response.data.data[0].url);
     } catch (error) {
-      console.error('Error al generar la imagen:', error);
+      console.error('Error al generar la imagen editada:', error);
     }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="file" accept="image/png" onChange={handleChange} />
-        <button type="submit">Generar variación de imagen</button>
+        <input type="file" accept="image/png" onChange={handleImageChange} />
+        <input type="file" accept="image/png" onChange={handleMaskChange} />
+        <input type="text" value={prompt} onChange={handlePromptChange} maxLength="1000" placeholder="Escribe una descripción" />
+        <button type="submit">Generar imagen editada</button>
       </form>
-      {generatedImage && (
+      {editedImage && (
         <div>
-          <h2>Imagen generada:</h2>
-          <img src={generatedImage} alt="Imagen generada" />
+          <h2>Imagen editada:</h2>
+          <img src={editedImage} alt="Imagen editada" />
         </div>
       )}
     </div>
   );
 };
 
-export default ImageVariation;
+export default ImageEdit;
