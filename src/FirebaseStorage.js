2142f97b050e1d70adc372e8e52f5585a63cddec
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-05 15:20:47 -0400

    asd2

--- src/FirebaseStorage.js
+++ src/FirebaseStorage.js
@@ -1,10 +1,15 @@
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { storage } from '../firebase/firebaseConfig';
-
+import { storage } from './firebaseConfig';
 
 export const uploadImage = async (file) => {
   const storageRef = ref(storage, `images/${file.name}`);
   const snapshot = await uploadBytes(storageRef, file);
   const downloadURL = await getDownloadURL(snapshot.ref);
   return downloadURL;
+};
+
+export const saveChatLog = async (chatLog) => {
+  const storageRef = ref(storage, 'chat-logs');
+  const chatLogRef = ref(storageRef, `${Date.now()}.json`);
+  await uploadBytes(chatLogRef, new Blob([JSON.stringify(chatLog)], { type: 'application/json' }));
 };
