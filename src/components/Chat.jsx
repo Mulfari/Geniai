027ba7bbad3133be71cd174cd6dc5b2d7cf0d31a
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-05 15:16:53 -0400

    as2

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './styles.css/Chat.css';
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import "./styles.css/Chat.css";
 
 const ChatComponent = () => {
   const [message, setMessage] = useState("");
@@ -12,32 +14,50 @@ const ChatComponent = () => {
         "https://api.openai.com/v1/chat/completions",
         {
           model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: message }]
+          messages: [{ role: "user", content: message }],
         },
         {
           headers: {
             "Content-Type": "application/json",
-            'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-          }
+            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          },
         }
       );
-      setChatLog(prevState => [...prevState, { user: message, ai: response.data.choices[0].message.content }]);
+      const aiResponse = response.data.choices[0].message.content;
+      setChatLog((prevState) => [
+        ...prevState,
+        { user: message, ai: aiResponse },
+      ]);
       setMessage("");
+
+      // Guardar la conversación en Firebase
+      const chatRef = collection(db, "chatLogs");
+      await addDoc(chatRef, { user: message, ai: aiResponse });
     } catch (error) {
       console.log(error);
     }
   };
+
   return (
     <div className="chat-container">
       <div className="chat-box">
         {chatLog.map((log, index) => (
-          <div key={index} className={`chat-message ${log.user === "User" ? "user" : "ai"}`}>
+          <div
+            key={index}
+            className={`chat-message ${
+              log.user === "User" ? "user" : "ai"
+            }`}
+          >
             {log.user}: {log.ai}
           </div>
         ))}
       </div>
       <div className="chat-input">
-        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
         <button onClick={sendMessage}>Send</button>
       </div>
     </div>
