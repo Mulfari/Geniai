328dec691a2b1bb1efe9039d68e2c7e39c8865c1
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-05 16:34:17 -0400

    asd2

--- src/components/NewImage.jsx
+++ src/components/NewImage.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './styles.css/NewImage.css';
-
-
+import { uploadImageToFirebase } from '../FirebaseStorage';
 
 const NewImage = () => {
   const [description, setDescription] = useState('');
   const [imageSrc, setImageSrc] = useState(null);
-  const [additionalImages, setAdditionalImages] = useState([]);
   const [loading, setLoading] = useState(false);
-  
+
   const generateImage = async () => {
     setLoading(true);
 
@@ -27,7 +25,7 @@ const NewImage = () => {
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-          },     
+          },
         }
       );
 
@@ -39,37 +37,22 @@ const NewImage = () => {
     }
   };
 
-  const generateSimilarImages = async () => {
-    setLoading(true);
-    const generatedImages = [];
-
-    for (let i = 0; i < 3; i++) {
-      try {
-        const response = await axios.post(
-          'https://api.openai.com/v1/images/generations',
-          {
-            model: 'image-alpha-001',
-            prompt: description,
-            num_images: 1,
-            size: '512x512',
-            response_format: 'url',
-          },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-            },
-          }
-        );
-
-        generatedImages.push(response.data.data[0].url);
-      } catch (error) {
-        console.error('Error generating similar image:', error);
-      }
+  const saveGeneratedImage = async () => {
+    if (!imageSrc) {
+      alert('No hay imagen generada para guardar.');
+      return;
     }
 
-    setAdditionalImages(generatedImages);
-    setLoading(false);
+    const fileName = `generated-image-${Date.now()}.png`;
+    const response = await fetch(imageSrc);
+    const imageBlob = await response.blob();
+
+    try {
+      const downloadURL = await uploadImageToFirebase(fileName, imageBlob);
+      alert(`Imagen guardada en Firebase Storage. URL de descarga: ${downloadURL}`);
+    } catch (error) {
+      console.error('Error al guardar la imagen en Firebase Storage:', error);
+    }
   };
 
   return (
@@ -85,22 +68,10 @@ const NewImage = () => {
         {loading ? 'Generando...' : 'Generar imagen'}
       </button>
       {imageSrc && (
-        <button onClick={generateSimilarImages} disabled={loading}>
-          {loading ? 'Generando coincidencias...' : 'Generar coincidencias'}
-        </button>
-      )}
-      {imageSrc && (
         <div>
           <h2>Imagen generada:</h2>
           <img src={imageSrc} alt="Imagen generada" />
-        </div>
-      )}
-      {additionalImages.length > 0 && (
-        <div>
-                    <h2>Imágenes similares:</h2>
-          {additionalImages.map((src, index) => (
-            <img key={index} src={src} alt={`Imagen similar ${index + 1}`} />
-          ))}
+          <button onClick={saveGeneratedImage}>Descargar imagen generada</button>
         </div>
       )}
     </div>
